8fa8fce9e41455855822940bc3bb4887c32073b5
estefania-git <ecasmed@gmail.com>
2019-12-10 13:47:24 +0100

    second commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,14 @@
-import React from 'react';
+import React, { Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Switch, Route, Link, withRouter } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+class App extends Component {
+ render(){
+    return (
+   <h1>Hola</h1>
   );
+ }
 }
 
-export default App;
+export default withRouter(App);
