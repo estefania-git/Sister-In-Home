9094000627f6f71a888ab2aa017e0eb1c1c27ddd
estefania-git <ecasmed@gmail.com>
2019-12-19 14:21:07 +0100

    start fixed

--- server/app.js
+++ server/app.js
@@ -48,7 +48,7 @@ app.use(cookieParser());
 
 // app.set('views', path.join(__dirname, 'views'));
 // app.set('view engine', 'hbs');
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // default value for title local
 
